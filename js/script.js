a72c500031ae6b608f9481586085e3a797fb0b91
Nikita Smirnov <nikita_smirnov91@mail.ru>
2026-01-18 21:50:53 +0300

    added a hero section and a folder with images for the slider

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  loop: true,
+  speed: 1000,
+  effect: 'fade',
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
