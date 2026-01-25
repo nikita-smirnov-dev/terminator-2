7c71e35258cf6f91f72672a31d25d8796e8b464b
Nikita Smirnov <nikita_smirnov91@mail.ru>
2026-01-25 15:31:02 +0300

    added sections plot and terminators

--- js/script.js
+++ js/script.js
@@ -8,3 +8,29 @@ const swiper = new Swiper('.swiper', {
     disableOnInteraction: false,
   },
 });
+
+const tab800 = document.getElementById('t-800');
+const tab1000 = document.getElementById('t-1000');
+
+const t800 = document.querySelector('.terminator__info--t800');
+const t1000 = document.querySelector('.terminator__info--t1000');
+
+tab800.classList.add('active');
+t800.classList.add('is-active');
+t1000.classList.remove('is-active');
+
+tab800.addEventListener('click', () => {
+  tab800.classList.add('active');
+  tab1000.classList.remove('active');
+
+  t800.classList.add('is-active');
+  t1000.classList.remove('is-active');
+});
+
+tab1000.addEventListener('click', () => {
+  tab1000.classList.add('active');
+  tab800.classList.remove('active');
+
+  t1000.classList.add('is-active');
+  t800.classList.remove('is-active');
+});
